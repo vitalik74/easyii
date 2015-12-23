1e0226f772f69f7adc14036984527ff9333768db
Vitalik <tsibikov_vit@mail.ru>
2015-12-23 14:41:15 +0500

    Set expire cookie

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends \yii\easyii\components\Controller
 
     public function actionChangeSite($domain)
     {
-        setcookie(Multisite::COOKIE_DOMAIN_KEY, strtolower($domain));
+        setcookie(Multisite::COOKIE_DOMAIN_KEY, strtolower($domain), time() + 24 * 3600);
 
         return $this->redirect(['index']);
     }
