814dfbe9764cc0bc4470f226aa966842c3f6f664
Vitalik <tsibikov_vit@mail.ru>
2016-01-13 18:21:09 +0500

    Add link from slug in text module

--- modules/text/views/a/index.php
+++ modules/text/views/a/index.php
@@ -30,7 +30,7 @@ $module = $this->context->module->id;
                 <?php endif; ?>
                 <td><a href="<?= Url::to(['/admin/'.$module.'/a/edit', 'id' => $item->primaryKey]) ?>"><?= $item->text ?></a></td>
                 <?php if(IS_ROOT) : ?>
-                    <td><?= $item->slug ?></td>
+                    <td><a href="<?= Url::to(['/admin/'.$module.'/a/edit', 'id' => $item->primaryKey]) ?>"><?= $item->slug ?></a></td>
                     <td><a href="<?= Url::to(['/admin/'.$module.'/a/delete', 'id' => $item->primaryKey]) ?>" class="glyphicon glyphicon-remove confirm-delete" title="<?= Yii::t('easyii', 'Delete item') ?>"></a></td>
                 <?php endif; ?>
             </tr>
